b550e8d6bda97b93f2386ac8ab787ec75d3227c5
Polina <khartanovichp@mail.ru>
2022-12-10 05:49:46 +0300

    fix: file system in tests

--- Lab5/Backups.Extra.Test/BackupsExtraTests.cs
+++ Lab5/Backups.Extra.Test/BackupsExtraTests.cs
@@ -11,20 +11,20 @@ public class BackupsExtraTests
     public void ControlRestorePointsAmount()
     {
         string generalPath = @"/home/";
-        BackupObject obj1 = new BackupObject(@"/bin/link");
-        BackupObject obj2 = new BackupObject(@"/bin/zsh");
+        BackupObject obj1 = new BackupObject(@"/bin/cat");
+        BackupObject obj2 = new BackupObject(@"/bin/date");
         BackupExtraTask task1 = new BackupExtraTask("task 1", generalPath);
         task1.SetTypeOfLogging(new FileLogger(), false);
-        task1.SetRepository(new FileSystemRepository(new PhysicalFileSystem()));
+        task1.SetRepository(new InMemoryRepository(new MemoryFileSystem()));
         task1.SetStorageAlgorithm(new SplitSaver());
         task1.AddObject(obj1);
         task1.AddObject(obj2);
         task1.AddPoint();
-        BackupObject obj3 = new BackupObject(@"/bin/unlink");
+        BackupObject obj3 = new BackupObject(@"/bin/pax");
         task1.AddObject(obj3);
         task1.AddPoint();
         task1.SetRemovalPolicy(new AmountLimitAlgo(1));
-        task1.SetPolicyForSystemRemoval(new FileSystemRemover());
+        task1.SetPolicyForSystemRemoval(new InMemoryRemover());
         task1.ClearRestorePoints();
         task1.Serialize();
         Assert.Equal(1, task1.Backup.GetPoints().Count);
